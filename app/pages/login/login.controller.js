878c9bb84ec2b06a51d58c673752e1d908e73ce8
shumik-vladyslav <shumik.vladyslav@gmail.com>
2016-07-19 13:35:53 +0300

    f

--- app/pages/login/login.controller.js
+++ app/pages/login/login.controller.js
@@ -12,14 +12,14 @@ function LoginController($scope, $http, $httpParamSerializerJQLike) {
         console.log($scope.password)
         console.log(JSON.stringify({ password: $scope.password }))
         console.log('{"password": "$scope.password}"')
-        var url = 'https://dev.officient.dk/users/' + $scope.username + '/login';
+        var url = 'http://dev.officient.dk/lara/public/api/auth/login';
 
         $http({
             method: 'POST',
             url: url,
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             responseType: "json",
-            data: $httpParamSerializerJQLike({ password: $scope.password }),
+            data: $httpParamSerializerJQLike({ password: $scope.password, email: $scope.username }),
         }).success(function (req) {
             var user = angular.fromJson(req);
             console.log(user);
@@ -28,7 +28,7 @@ function LoginController($scope, $http, $httpParamSerializerJQLike) {
             $.cookie('user', user);
 
             //if (user.userID)
-                window.location.href = 'http://dev.officient.dk/create.html';
+               // window.location.href = 'http://dev.officient.dk/create.html';
                 //window.location.href = 'http://192.168.1.118:8081/create.html';
         });
 
